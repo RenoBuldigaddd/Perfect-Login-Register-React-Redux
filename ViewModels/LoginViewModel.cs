f2a8a82338b4fcd2782d3551d1c7a024b2c32dc8
Anonymous <user@rivne.itstep.org>
2019-11-02 10:10:38 +0200

    First commit, perfect backend + frontend

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace React_Login_Register_Redux.ViewModels
+{
+    public class LoginViewModel
+    {
+        [EmailAddress(ErrorMessage = "There should be your email!")]
+        [Required(ErrorMessage = "Field sould not be empty!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Field sould not be empty!")]
+        public string Password { get; set; }
+    }
+}
